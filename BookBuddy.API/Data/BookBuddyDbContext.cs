6679d40425bc33cfb22cf35f4660c77d6f2d5689
Kedar Kawale <kedarkawale003@gmail.com>
2026-02-16 22:43:02 +0530

    DbContext class created with DbSet<> properties

---
+++ BookBuddy.API/Data/BookBuddyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookBuddy.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BookBuddy.API.Data  // this is nothing but the Organization-"Groups related classes together" 
+                              //Matches your folder structure
+                              //Prevents naming conflicts
+{ 
+        public class BookBuddyDbContext : DbContext
+        {
+            public BookBuddyDbContext(DbContextOptions<BookBuddyDbContext> options) : base(options)  // ← This passes options to parent DbContext
+            {
+            //the body can be empty -no other initialization needed.  
+            }
+
+        // below our DbSet prop goes 
+        public DbSet<User> Userss { get; set; }  // Userss, Rentalss... are Prop names. in EF core naming convention we  widely use plural names as per our classes.
+        public DbSet<Rentals> Rentalss { get; set; }
+        public DbSet<Payments> Paymentss { get; set; }
+        public DbSet<Book> Bookss { get; set; }
+    }
+}
+
--- BookBuddy.API/obj/Debug/net8.0/BookBuddy.API.AssemblyInfo.cs
+++ BookBuddy.API/obj/Debug/net8.0/BookBuddy.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookBuddy.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0465cfd2340b511833f02fddcb3a26d3d428286c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookBuddy.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookBuddy.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
